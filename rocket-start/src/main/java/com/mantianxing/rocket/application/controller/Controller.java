1f46701b408ef8e55e89bd80ca079d0f3d6996cc
chuanlong <yuchuan0808@gmail.com>
2024-08-18 14:29:18 +0800

    接入gpt

--- rocket-start/src/main/java/com/mantianxing/rocket/application/controller/Controller.java
+++ rocket-start/src/main/java/com/mantianxing/rocket/application/controller/Controller.java
@@ -1,11 +1,14 @@
 package com.mantianxing.rocket.application.controller;
 
+import com.mantianxing.rocket.application.server.agent.ChatAgentFactory;
+import com.mantianxing.rocket.application.server.agent.CommentAgent;
 import com.mantianxing.rocket.application.server.dataObject.CommentsDO;
 import com.mantianxing.rocket.application.server.mapper.CommentsMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 import java.util.Optional;
 
@@ -14,6 +17,9 @@ public class Controller {
     @Autowired
     private CommentsMapper commentsMapper;
 
+    @Autowired
+    private HttpServletRequest request;
+
     @RequestMapping("/testMysql")
     @ResponseBody
     public CommentsDO comments() {
@@ -21,4 +27,14 @@ public class Controller {
         return Optional.ofNullable(comments).map(it ->it.get(0)).orElse(null);
     }
 
+    @RequestMapping("/testGpt")
+    @ResponseBody
+    public String gpt() {
+        String content = request.getParameter("content");
+
+        String apiKey = request.getParameter("apiKey");
+
+        return ChatAgentFactory.chatAgent(CommentAgent.class, apiKey)
+                .chat(content);
+    }
 }
